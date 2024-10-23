03467e5f9d96aaa3ba342e5a8f650058a9c40ea3
Travis Smith <balloondude2@gmail.com>
2024-10-22 22:19:44 -0600

    hardcode poc

--- src/public/libultra/voice.cpp
+++ src/public/libultra/voice.cpp
@@ -49,7 +49,7 @@ extern "C" {
 // };
 
 int32_t osVoiceInit(OSMesgQueue* mq, OSVoiceHandle* hd, int channel) {
-    LUSLOG_DEBUG("osVoiceInit, channel: %x", channel);
+    LUSLOG_DEBUG("osVoiceInit, channel (ctrlr port): %x", channel);
     return 0;
 
 //     s32 errorCode;
@@ -106,42 +106,65 @@ int32_t osVoiceInit(OSMesgQueue* mq, OSVoiceHandle* hd, int channel) {
 }
 
 int32_t osVoiceSetWord(OSVoiceHandle* hd, u8* word) {
-    LUSLOG_DEBUG("osVoiceSetWord", NULL);
+    // LUSLOG_DEBUG("osVoiceSetWord", NULL);
     return 0;
 }
 
 int32_t osVoiceCheckWord(u8* word) {
-    LUSLOG_DEBUG("osVoiceCheckWord", NULL);
+    // LUSLOG_DEBUG("osVoiceCheckWord", NULL);
     return 0;
 }
 
 int32_t osVoiceStartReadData(OSVoiceHandle* hd) {
-    LUSLOG_DEBUG("osVoiceStartReadData", NULL);
+    // LUSLOG_DEBUG("osVoiceStartReadData", NULL);
     return 0;
 }
 
 int32_t osVoiceStopReadData(OSVoiceHandle* hd) {
-    LUSLOG_DEBUG("osVoiceStopReadData", NULL);
+    // LUSLOG_DEBUG("osVoiceStopReadData", NULL);
     return 0;
 }
 
+/*
+typedef struct {
+    / 0x00 / u16 warning;       // Warning
+    / 0x02 / u16 answerNum;     // Candidate number (0~5)
+    / 0x04 / u16 voiceLevel;    // Voice input level
+    / 0x06 / u16 voiceRelLevel; // Relative voice level "voice_sn"
+    / 0x08 / u16 voiceTime;     // Voice input time
+    / 0x0A / u16 answer[5];     // Candidate word number
+    / 0x14 / u16 distance[5];   // Distance value
+} OSVoiceData; // size = 0x20
+*/
+
+// dist, ansNum,   warn,        voice, voiceRel
+// (800, 2, VOICE_WARN_TOO_SMALL, 500, 2000);
+
 int32_t osVoiceGetReadData(OSVoiceHandle* hd, OSVoiceData* result) {
     LUSLOG_DEBUG("osVoiceGetReadData", NULL);
+    // Hardcoded pictograph command
+    // Implement voice recognition stuff to set this data. 
+    result->warning = 0;
+    result->answerNum = 0;
+    result->voiceLevel = 1000;
+    result->voiceRelLevel = 2500;
+    result->answer[0] = 1; //VOICE_WORD_ID_CHEESE;
+    result->distance[0] = 10;
     return 0;
 }
 
 int32_t osVoiceClearDictionary(OSVoiceHandle* hd, u8 numWords) {
-    LUSLOG_DEBUG("osVoiceClearDictionary", NULL);
+    // LUSLOG_DEBUG("osVoiceClearDictionary", NULL);
     return 0;
 }
 
 int32_t osVoiceMaskDictionary(OSVoiceHandle* hd, u8* maskPattern, int size) {
-    LUSLOG_DEBUG("osVoiceMaskDictionary", NULL);
+    // LUSLOG_DEBUG("osVoiceMaskDictionary", NULL);
     return 0;
 }
 
 int32_t osVoiceControlGain(OSVoiceHandle* hd, s32 analog, s32 digital) {
-    LUSLOG_DEBUG("osVoiceControlGain", NULL);
+    // LUSLOG_DEBUG("osVoiceControlGain", NULL);
     return 0;
 }
 
